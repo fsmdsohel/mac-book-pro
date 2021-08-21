479ac1381914a707dd786b2743af03861a31359a
Sohel Rana <73012378+fsmdsohel@users.noreply.github.com>
2021-08-21 20:38:34 +0600

    first commit

--- app.js
+++ app.js
@@ -1,119 +1,93 @@
 function totalPriceCommonFun() {
-    const mainCost = document.getElementById('main-cost').innerText;
-    const memoryCost = document.getElementById('extra-memoryCost').innerText;
-    const storageCost = document.getElementById('storage-cost').innerText;
-    const deliveCost = document.getElementById('delivery-cost').innerText;
+  const mainCost = document.getElementById("main-cost").innerText;
+  const memoryCost = document.getElementById("extra-memoryCost").innerText;
+  const storageCost = document.getElementById("storage-cost").innerText;
+  const deliveCost = document.getElementById("delivery-cost").innerText;
 
-    const totalPrice = parseInt(mainCost) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveCost);
+  const totalPrice =
+    parseInt(mainCost) +
+    parseInt(memoryCost) +
+    parseInt(storageCost) +
+    parseInt(deliveCost);
 
-    const total = document.getElementById('total-price').innerText = totalPrice;
+  document.getElementById("total-price").innerText = totalPrice;
 
-    // start bonus part
-    const totalBonus = document.getElementById('totalPrice-bonus');
-    totalBonus.innerText = total;
+  // start bonus part
+  document.getElementById("totalPrice-bonus").innerText = totalPrice;
 
-    // pin matched
-
-
-
+  // pin matched
 }
 
-function discount() {
-    const mainCost = document.getElementById('main-cost').innerText;
-    const memoryCost = document.getElementById('extra-memoryCost').innerText;
-    const storageCost = document.getElementById('storage-cost').innerText;
-    const deliveCost = document.getElementById('delivery-cost').innerText;
+document.getElementById("8gb-best").addEventListener("click", function () {
+  let normalMalMemory = document.getElementById("extra-memoryCost");
 
-    const totalPrice = parseInt(mainCost) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveCost);
-
-    const total = document.getElementById('total-price').innerText = totalPrice;
-
-
-
-
-
-}
-
-
-document.getElementById('8gb-best').addEventListener('click', function () {
-
-    let normalMalMemory = document.getElementById('extra-memoryCost');
-
-    normalMalMemory.innerText = 0;
-    totalPriceCommonFun()
-
+  normalMalMemory.innerText = 0;
+  totalPriceCommonFun();
 });
 
-document.getElementById('16gb-memory').addEventListener('click', function () {
+document.getElementById("16gb-memory").addEventListener("click", function () {
+  var extraMemry = document.getElementById("extra-memoryCost");
 
-    var extraMemry = document.getElementById('extra-memoryCost');
-
-    extraMemry.innerText = 180;
-    totalPriceCommonFun()
-
+  extraMemry.innerText = 180;
+  totalPriceCommonFun();
 });
 
-document.getElementById('best-storage').addEventListener('click', function () {
-    var normalStorage = document.getElementById('storage-cost');
-    normalStorage.innerText = 0;
+document.getElementById("best-storage").addEventListener("click", function () {
+  var normalStorage = document.getElementById("storage-cost");
+  normalStorage.innerText = 0;
 
-    totalPriceCommonFun()
+  totalPriceCommonFun();
+});
 
-})
-
-document.getElementById('extra-storageCost').addEventListener('click', function () {
-    var extraStorage = document.getElementById('storage-cost');
+document
+  .getElementById("extra-storageCost")
+  .addEventListener("click", function () {
+    var extraStorage = document.getElementById("storage-cost");
 
     extraStorage.innerText = 100;
 
-    totalPriceCommonFun()
+    totalPriceCommonFun();
+  });
+
+document.getElementById("1tb-extra").addEventListener("click", function () {
+  var extraStorage = document.getElementById("storage-cost");
+
+  extraStorage.innerText = 180;
+
+  totalPriceCommonFun();
 });
 
-document.getElementById('1tb-extra').addEventListener('click', function () {
-    var extraStorage = document.getElementById('storage-cost');
+document.getElementById("shipping-cost").addEventListener("click", function () {
+  var normalDelivery = document.getElementById("delivery-cost");
 
-    extraStorage.innerText = 180;
+  normalDelivery.innerText = 0;
 
-    totalPriceCommonFun()
-
-
-})
-
-document.getElementById('shipping-cost').addEventListener('click', function () {
-    var normalDelivery = document.getElementById('delivery-cost');
-
-    normalDelivery.innerText = 0;
-
-    totalPriceCommonFun()
-
+  totalPriceCommonFun();
 });
 
-document.getElementById('extradelvery-charge').addEventListener('click', function () {
-    var normalDelivery = document.getElementById('delivery-cost');
+document
+  .getElementById("extradelvery-charge")
+  .addEventListener("click", function () {
+    var normalDelivery = document.getElementById("delivery-cost");
 
     normalDelivery.innerText = 20;
 
-    totalPriceCommonFun()
+    totalPriceCommonFun();
+  });
 
+document
+  .getElementById("apply-discount")
+  .addEventListener("click", function () {
+    const cuponInput = document.getElementById("cupon-input");
+    const totalPrice = document.getElementById("total-price").innerText;
+    const totalPriceValue = document.getElementById("totalPrice-bonus");
 
-
-});
-
-document.getElementById('apply-discount').addEventListener('click', function () {
-
-
-    discount()
-
-    // const discount = document.getElementById('discount');
-    // const discountMached = discount.value;
-
-    // if (discountMached == 'stevekaku') {
-    //     let matched = totalPriceCommonFun() / 20;
-
-    //     totalBonus.innerText = matched;
-    // } else {
-    //     totalPriceCommonFun()
-    // }
-})
-
-
+    if (cuponInput.value == "stevekaku") {
+      const discount = (totalPrice * 20) / 100;
+      console.log(discount);
+      totalPriceValue.innerText = parseInt(totalPrice) - discount;
+    } else {
+      alert("Invalid cupon");
+    }
+    cuponInput.value = "";
+  });
